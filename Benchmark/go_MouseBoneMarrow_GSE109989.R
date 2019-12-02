e0fc119fd5d1a06d985370dca514e44a88ce7fb3
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-02 13:38:54 -0500

    Update go_MouseBoneMarrow_GSE109989.R

--- Benchmark/go_MouseBoneMarrow_GSE109989.R
+++ Benchmark/go_MouseBoneMarrow_GSE109989.R
@@ -3,10 +3,8 @@ source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 ########################################
 setwd('F:/AIM/MouseBoneMarrow_GSE109989/')
 
-DATA=read.table(file='count_matrix.csv',sep='\t',header=TRUE,row.names=1)
+DATA=read.csv(file='count_matrix.csv',sep=',',header=TRUE,row.names=1)
 saveRDS(DATA,file='DATA.RDS')
-
-
 
 
 pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
@@ -18,4 +16,35 @@ pbmc <- ScaleData(pbmc, features = all.genes)
 pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 50)
 pbmc <- RunUMAP(pbmc, dims = 1:50)
 DimPlot(pbmc, reduction = "umap")
+saveRDS(pbmc,file='pbmc.RDS')
+
+
+
+
+###########################
+setwd('F:/AIM/MouseBoneMarrow_GSE109989/')
+pbmc=readRDS(file='pbmc.RDS')
+
+FeaturePlot(pbmc, features=c('Cenpa','Cd79a'))
+
+VEC=pbmc@reductions$umap@cell.embeddings
+rownames(VEC)=colnames(pbmc)
+PCA= pbmc@reductions$pca@cell.embeddings
+#######################
+
+OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+VALUE=vector.getValue(PCA)
+OUT=vector.gridValue(OUT,VALUE, SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+#OUT=vector.selectCenter(OUT)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
+
+#######################
+OUT=vector.reDrawArrow(OUT, COL=OUT$COL)
+OUT=vector.selectRegion(OUT)
+
+
+
+
 
