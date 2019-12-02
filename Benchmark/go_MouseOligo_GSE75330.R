5b0f67b95c438a89dd641c8458e108a7535e7164
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-02 15:01:05 -0500

    Update go_MouseOligo_GSE75330.R

--- Benchmark/go_MouseOligo_GSE75330.R
+++ Benchmark/go_MouseOligo_GSE75330.R
@@ -1,1 +1,18 @@
-setwd('F:\Vector\data\MouseOligo_GSE75330')
+
+setwd('F:/Vector/data/MouseOligo_GSE75330')
+DATA=read.table(file='GSE75330_Marques_et_al_mol_counts2.tab',sep='\t',header=TRUE,row.names=1)
+saveRDS(DATA,file='DATA.RDS')
+
+
+############################################
+pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 50)
+pbmc <- RunUMAP(pbmc, dims = 1:50)
+DimPlot(pbmc, reduction = "umap")
+saveRDS(pbmc,file='pbmc.RDS')
+
+
