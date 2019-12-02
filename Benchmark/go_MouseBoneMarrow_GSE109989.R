045c1a7029c1a027934abe8110e0cdd40b9c402f
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-02 13:58:53 -0500

    Update go_MouseBoneMarrow_GSE109989.R

--- Benchmark/go_MouseBoneMarrow_GSE109989.R
+++ Benchmark/go_MouseBoneMarrow_GSE109989.R
@@ -10,7 +10,7 @@ saveRDS(DATA,file='DATA.RDS')
 pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
 #pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
-pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
 all.genes <- rownames(pbmc)
 pbmc <- ScaleData(pbmc, features = all.genes)
 pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 50)
@@ -25,14 +25,14 @@ saveRDS(pbmc,file='pbmc.RDS')
 setwd('F:/AIM/MouseBoneMarrow_GSE109989/')
 pbmc=readRDS(file='pbmc.RDS')
 
-FeaturePlot(pbmc, features=c('Cenpa','Cd79a'))
+FeaturePlot(pbmc, features=c('Gfi1','Cenpa','Cd79a','Mpeg1','Cd47'))
 
 VEC=pbmc@reductions$umap@cell.embeddings
 rownames(VEC)=colnames(pbmc)
 PCA= pbmc@reductions$pca@cell.embeddings
 #######################
 
-OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
+OUT=vector.buildGrid(VEC, N=50,SHOW=TRUE)
 OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
 VALUE=vector.getValue(PCA)
 OUT=vector.gridValue(OUT,VALUE, SHOW=TRUE)
@@ -43,6 +43,8 @@ OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
 #######################
 OUT=vector.reDrawArrow(OUT, COL=OUT$COL)
 OUT=vector.selectRegion(OUT)
+OUT=vector.reDrawRegion(OUT)
+
 
 
 
