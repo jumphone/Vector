6e18e3a5aae6f42967bfc14b852cb774e8de17e9
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-02 20:56:51 -0500

    Update go_MouseOligo_GSE75330.R

--- Benchmark/go_MouseOligo_GSE75330.R
+++ Benchmark/go_MouseOligo_GSE75330.R
@@ -3,12 +3,31 @@ setwd('F:/Vector/data/MouseOligo_GSE75330')
 DATA=read.table(file='GSE75330_Marques_et_al_mol_counts2.tab',sep='\t',header=TRUE,row.names=1)
 saveRDS(DATA,file='DATA.RDS')
 
+L=readRDS('LABEL.RDS')
+L=L[(length(L)-5069+1):length(L)]
+library(stringr)
+L=str_replace(L, '_batch2', '')
+saveRDS(L,file='GSE75330.LABEL.RDS')
+
 
 ############################################
-
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 setwd('F:/Vector/data/MouseOligo_GSE75330')
 DATA=readRDS('DATA.RDS')
 pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
+pbmc@meta.data$type=readRDS('GSE75330.LABEL.RDS')
+pbmc@meta.data$type[which(pbmc@meta.data$type=='Differentiation-committed oligodendrocyte precursors')]='Differentiation-committed OPC'
+
+
+############################################
+used_cell_index=which( pbmc@meta.data$type != 'PPR')
+
+DATA=DATA[,used_cell_index]
+TYPE=pbmc@meta.data$type[used_cell_index]
+pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
+pbmc@meta.data$type=TYPE
+##########################
+
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
 all.genes <- rownames(pbmc)
@@ -18,13 +37,8 @@ pbmc <- RunUMAP(pbmc, dims = 1:50)
 DimPlot(pbmc, reduction = "umap")
 saveRDS(pbmc,file='pbmc.RDS')
 
+###################
 
-############################################
-L=readRDS('LABEL.RDS')
-L=L[(length(L)-5069+1):length(L)]
-library(stringr)
-L=str_replace(L, '_batch2', '')
-saveRDS(L,file='GSE75330.LABEL.RDS')
 
 
 #############################
@@ -32,9 +46,6 @@ source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 
 setwd('F:/Vector/data/MouseOligo_GSE75330')
 pbmc=readRDS('pbmc.RDS')
-
-pbmc@meta.data$type=readRDS('GSE75330.LABEL.RDS')
-pbmc@meta.data$type[which(pbmc@meta.data$type=='Differentiation-committed oligodendrocyte precursors')]='Differentiation-committed OPC'
 
 
 DimPlot(pbmc, group.by='type',label = TRUE,
@@ -60,14 +71,24 @@ dev.off()
 
 
 VCOL=vector.vcol(pbmc@meta.data$score, c(min(pbmc@meta.data$score),
-                                         median(pbmc@meta.data$score),
-                                         max(pbmc@meta.data$score)),c('blue3','grey80','red3'))
+                 median(pbmc@meta.data$score), max(pbmc@meta.data$score)),
+                 c('blue3','grey80','red3'))
 pdf('f2.pdf',width=3.5,height=4)
 plot(VEC,col=VCOL,pch=16,cex=0.2)
 dev.off()
 
 
 ###########################################################
+
+
+
+
+
+
+
+
+
+
 
 
 
