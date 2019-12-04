d207b62e80c4b3e21d3c2c232042e8503e924ed7
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-03 20:19:12 -0500

    Update go_MouseIntestine_GSE92332.R

--- Benchmark/go_MouseIntestine_GSE92332.R
+++ Benchmark/go_MouseIntestine_GSE92332.R
@@ -35,65 +35,66 @@ pbmc <- ScaleData(pbmc, features = all.genes)
 
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
 
-pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs=50)
-pbmc <- RunUMAP(pbmc, dims = 1:50)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs=100)
+pbmc <- RunUMAP(pbmc, dims = 1:100)
 DimPlot(pbmc, reduction = "umap")
+
+
+FeaturePlot(pbmc,features='Lgr5')
 
 
 VEC=pbmc@reductions$umap@cell.embeddings
 rownames(VEC)=colnames(pbmc)
-PCA= pbmc@reductions$pca@cell.embeddings
+PCA= pbmc@reductions$pca@cell.embeddings#[,1:100]#[,50:100]#[,30:50]
+
+eigs <- apply(PCA,2,var)
 
 
 
 
-OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
+
+library(gmodels)
+D=as.matrix(pbmc@assays$RNA@scale.data)
+PCA.OUT=fast.prcomp(t(D), retx = TRUE, center = FALSE, scale. = FALSE, tol = NULL)
+N=min(which( cumsum(PCA.OUT$sdev^2)/sum(PCA.OUT$sdev^2) > 0.7))
+
+
+
+
+#N=300
+
+#sum(eigs[1:N]) / (sum(eigs[1:N])+(eigs[N]+0)*(length(VariableFeatures(pbmc))-N)/2)
+
+PCA=PCA.OUT$x[,1:N]
+
+
+
+OUT=vector.buildGrid(VEC, N=40,SHOW=TRUE)
 OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
 
-SCORE=vector.calScore(OUT,PCA,SHOW=TRUE)
-#VALUE=SCORE#vector.getScore(PCA)
-OUT=vector.gridValue(OUT,SCORE, SHOW=TRUE)
+#TMP=apply(PCA,2,cor,OUT$VALUE,method='spearman')
+#OUT$VALUE=OUT$VALUE-predict(lm(OUT$VALUE~apply(OUT$PCA,2,.normX)))
+
+
+#OUT$VALUE=predict(lm(OUT$VALUE~apply(OUT$PCA,2,.normX)))
+#OUT=vector.getValue(OUT, PCA[,USED], SHOW=TRUE)
+
+OUT=vector.gridValue(OUT,SHOW=TRUE)
 OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
-#OUT=vector.autoCenterNew(OUT,SHOW=TRUE)
-#OUT=vector.selectCenter(OUT)
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
 
 
 
-FeaturePlot(pbmc,features=c('Bub1','Cdk1','Nusap1','Hist1h2al','Cdt1','Orc1','Lgr5'),ncol=3,order=TRUE)
 
-
-pbmc@meta.data$type=TYPE
-DimPlot(pbmc,group.by='type')
-
-
-DimPlot(pbmc, reduction = "pca",dims=c(1,2),group.by='type')
+####################################################################
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+########################################
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+setwd('F:/Vector/data/MouseIntestine_GSE92332/')
 
 
 DATA=readRDS('DATA.RDS')
@@ -126,6 +127,20 @@ DATA=DATA[,used_cell_index]
 TYPE=TYPE[used_cell_index]
 BATCH=BATCH[used_cell_index]
 ####################################################
+
+mybeer=BEER(DATA, BATCH, GNUM=30, PCNUM=150, ROUND=1, GN=5000, SEED=1, COMBAT=TRUE )
+
+
+
+
+
+
+
+
+
+
+
+
 
 pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
 
