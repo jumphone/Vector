8f7565bdc7eddecd45ba3bae403629b5d01fd558
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-03 23:38:43 -0500

    Update go_MouseDentateGyrus_GSE104323.R

--- Benchmark/go_MouseDentateGyrus_GSE104323.R
+++ Benchmark/go_MouseDentateGyrus_GSE104323.R
@@ -5,36 +5,42 @@ setwd('F:/AIM/MouseDentateGyrus_GSE104323/')
 
 pbmc=readRDS(file='pbmc.RDS')
 
-
+#PCA.OUT=vector.SeuratRandomPCA(pbmc)
+#PCA=PCA.OUT$PRED.PCA[,1:PCA.OUT$N]
 #umap=BEER.bbknn(pbmc, PCUSE, NB=2, NT=10)
 #pbmc@reductions$umap@cell.embeddings=umap
 #DimPlot(pbmc, reduction.use='umap', group.by='batch', pt.size=0.1,label=F)
 
+
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 150)
+pbmc <- RunUMAP(pbmc, dims = 1:150,n.components = 20)
+
+UMAP= pbmc@reductions$umap@cell.embeddings
+
+DimPlot(pbmc, reduction = "umap")
 DimPlot(pbmc, group.by='type',label = TRUE)+NoLegend()
 #FeaturePlot(pbmc,features=c('Nes','Olig2','Pdgfra','Gfap','Gdnf'))
 #######################################
+
+pbmc=readRDS(file='pbmc.RDS')
 
 
 VEC=pbmc@reductions$umap@cell.embeddings
 rownames(VEC)=colnames(pbmc)
 PCA= pbmc@reductions$pca@cell.embeddings
+#PCA=UMAP
 #######################
 
 
 OUT=vector.buildGrid(VEC, N=40,SHOW=TRUE)
 OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
 
 
-SCORE=pbmc@assays$RNA@data[which(rownames(pbmc)=='Nes'),]
-#SCORE=vector.getValue(PCA)#vector.calScore(OUT,PCA,SHOW=TRUE)
-
-
-#VALUE=SCORE#vector.getScore(PCA)
-OUT=vector.gridValue(OUT,SCORE, SHOW=TRUE)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
 OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
-#OUT=vector.autoCenterNew(OUT,SHOW=TRUE)
-#OUT=vector.selectCenter(OUT)
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
+
 
 
 
