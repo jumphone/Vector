b171b1e9832b4231ec19d568f264c56094f5c743
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-04 22:32:27 -0500

    Update go_MouseOligo_GSE75330.R

--- Benchmark/go_MouseOligo_GSE75330.R
+++ Benchmark/go_MouseOligo_GSE75330.R
@@ -122,140 +122,12 @@ DrawHeatMap(TAG)
 
 
 ########################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-R.PCA=apply(PCA,2,rank)
-THIS.INDEX=which(pbmc@meta.data$type==TAG)
-THIS.R.PCA=R.PCA[THIS.INDEX,]
-
-
-MAT=matrix(0,nrow=ncol(PCA),ncol=nrow(PCA))
-rownames(MAT)=colnames(PCA)
-colnames(MAT)=paste0('R_',1:nrow(PCA))
-MAT[1:3,1:3]
-
-
-i=1
-while(i<=ncol(PCA)){
-    MAT[i,THIS.R.PCA[,i]]=1
-    i=i+1}
-
-library('ComplexHeatmap')
-library('circlize')
-library('seriation')
-
-mat=MAT
-o.mat=mat
-col_fun =colorRamp2(c(0,1), c('white','#000080'))
-
-Heatmap(o.mat,row_title='',name="",cluster_rows=FALSE,
-        cluster_columns=FALSE,show_heatmap_legend=FALSE，
-	show_column_dend = FALSE, show_row_dend = FALSE, 
-	show_column_names=FALSE, show_row_names=FALSE,
-	col=col_fun, border = TRUE
-	)
-tiff(paste0("IMG/",TAG,".HEAT.tiff"),width=6,height=2,units='in',res=600)
-Heatmap(o.mat,row_title='',name="",cluster_rows=FALSE,
-        cluster_columns=FALSE,show_heatmap_legend=FALSE，
-	show_column_dend = FALSE, show_row_dend = FALSE, 
-	show_column_names=FALSE, show_row_names=FALSE,
-	col=col_fun, border = TRUE
-	)
-dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DimPlot(pbmc, group.by='type',label = TRUE,
-        label.size=5,pt.size=0.5)+NoLegend()
-
-###################################
+# Draw rank curve
+
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+setwd('F:/Vector/data/MouseOligo_GSE75330')
+pbmc=readRDS('pbmc.RDS')
+#FeaturePlot(pbmc,features=c('Pdgfra','Bmp4','Sema4f','Mog'))
 
 VEC=pbmc@reductions$umap@cell.embeddings
 rownames(VEC)=colnames(pbmc)
@@ -263,195 +135,44 @@ PCA= pbmc@reductions$pca@cell.embeddings
 
 
 
-
-OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
-OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
-
-RPPCA=vector.RPPCA(PCA)
-
-OUT=vector.getValue(OUT, RPPCA[,1:50], SHOW=TRUE)
-OUT=vector.gridValue(OUT,SHOW=TRUE)
-
-OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
-
-
-OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
-
-eigs <- pbmc@reductions$pca@stdev^2
-
-
-
-N=200
-X=1:N
-fit=lm(eigs[1:N]~X+I(X^2))
-
-
-
-
-sum(eigs[1:N])/sum(VAR)
-(sum(eigs[1:N])+(eigs[N]+0)*(length(VariableFeatures(pbmc))-N)/2)
-
-
-
-
-
-
-OUT=vector.selectCenter(OUT)
-OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-OUT=vector.nonCenter(OUT)
-
-
-
-OUT=vector.drawArrow(OUT,P=1,SHOW=TRUE, COL=OUT$COL)
-
-
-
-
-
-
-
-
-
-
-OUT=vector.buildGrid(VEC, N=30,SHOW=TRUE)
-OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
-
-SCORE=vector.getValue(PCA)
-VALUE=SCORE#vector.getScore(PCA)
-
-OUT=vector.gridValue(OUT,VALUE, SHOW=TRUE)
-
-OUT=vector.autoCenterNew(OUT,SHOW=TRUE)
-#OUT=vector.selectCenter(OUT)
-OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
-
-
-
-
-
-
-GTEX=read.table('F:/Vector/data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct',header=TRUE,row.names=1,sep='\t')
-GTEX=GTEX[GTEX[,1] %in% names(which(table(GTEX[,1])==1)),]
-RN=GTEX[,1]
-GTEX=GTEX[,c(2:ncol(GTEX))]
-rownames(GTEX)=RN
-colnames(GTEX)=paste0('GTEX_',1:ncol(GTEX))
-
-
-saveRDS(GTEX,file='F:/Vector/data/GTEX.RDS')
-
-
-
-vector.scoreGTEX <- function(pbmc){
-    pbmc=pbmc
-    D1=as.matrix(pbmc@assays$RNA@data)
-    rownames(D1)=toupper(rownames(D1))
-    D1=D1[which(rownames(D1) %in% names(which(table(rownames(D1))==1))),]
-    colnames(D1)=paste0('D1_',colnames(D1))
-    DGTEX=.simple_combine(D1, GTEX)$combine
-    pbmc <- CreateSeuratObject(counts = DGTEX, project = "pbmc3k", min.cells = 0, min.features = 0)
-    pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
-    pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
-    pbmc <- ScaleData(pbmc, features = VariableFeatures(pbmc))
-    pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 50)
-    D1_INDEX=which(pbmc@meta.data$orig.ident=='D1')
-    D2_INDEX=which(pbmc@meta.data$orig.ident=='GTEX')
-    PCA=pbmc@reductions$pca@cell.embeddings
-    PCA.N=apply(PCA,2,rank)
-    MEAN=apply(PCA[D2_INDEX,],2,mean)
-    MEAN.MAT=matrix(rep(MEAN,each=nrow(PCA)),nrow=nrow(PCA))
-    SCORE=apply(apply(abs(PCA-MEAN.MAT),2,rank),1,mean)
-    SCORE=SCORE[D1_INDEX]
-    return(SCORE)
+TAG='OPC'
+R.PCA=apply(PCA,2,rank)
+THIS.INDEX=which(pbmc@meta.data$type==TAG)
+#THIS.R.PCA=R.PCA[THIS.INDEX,]
+
+MEAN=c()
+
+N=1
+PCA.RC=.normX(rank(PCA[,1]))
+PCA.RC=abs(PCA.RC-0.5)   
+VALUE=PCA.RC
+R.VALUE=rank(VALUE)/length(VALUE)
+this_mean=mean(R.VALUE[THIS.INDEX])
+
+MEAN=c(MEAN, this_mean)
+
+
+
+N=2
+while(N<=150){
+    PCA.RC=apply(apply(PCA[,1:N],2,rank), 2, .normX)
+    PCA.RC=abs(PCA.RC-0.5)   
+    VALUE=apply(PCA.RC,1,mean)
+    R.VALUE=rank(VALUE)/length(VALUE)
+    this_mean=mean(R.VALUE[THIS.INDEX])
+    MEAN=c(MEAN, this_mean)
+    print(N)
+    N=N+1
     }
 
-
-
-
-
-
-SD=apply(PCA[D2_INDEX,],2,sd)
-
-SD.MAT=matrix(rep(SD,each=nrow(PCA)),nrow=nrow(PCA))
-
-PCA.N=(PCA-MEAN.MAT)/SD.MAT
-
-
-
-PCA.N=()
-
-SCORE=apply(apply(abs(PCA.N),2,rank),1,mean)
-
-
-SCORE1=apply(apply(abs(PCA),2,rank),1,mean)
-
-
-pbmc1=readRDS('pbmc.RDS')
-pbmc@meta.data$orig.ident=as.character(pbmc@meta.data$orig.ident)
-pbmc@meta.data$orig.ident[D1_INDEX]=as.character(pbmc1@meta.data$type)
-
-
-
-
-DimPlot(pbmc,reductions='pca',dims=c(1,2),group.by='orig.ident')
-
-PCA=pbmc@reductions$pca@cell.embeddings[1:ncol(D1),]
-
-
-
-
-
-
-
-
-
-
-
-SCORE=apply(apply(abs(PCA),2,rank),1,mean)
-
-pbmc@meta.data$score=apply(apply(abs(PCA),2,rank),1,mean)
-
-
-LCOL=vector.lcol(pbmc@meta.data$type)
-pdf('f1.pdf',width=3.5,height=4)
-plot(VEC,col=LCOL,pch=16,cex=0.2)
+tiff(paste0("IMG/OPC.SCORE.tiff"),width=3,height=3,units='in',res=600)
+par(mar=c(2,2,2,2))
+plot(MEAN,type='l',lwd=5,ylim=c(min(MEAN),1))
 dev.off()
 
 
 
-VCOL=vector.vcol(pbmc@meta.data$score, c(min(pbmc@meta.data$score),quantile(pbmc@meta.data$score,0.1),
-                 median(pbmc@meta.data$score), quantile(pbmc@meta.data$score,0.9), max(pbmc@meta.data$score)),
-                 c('blue3','blue3','grey95','red3','red3'))
-pdf('f2.pdf',width=3.5,height=4)
-plot(VEC,col=VCOL,pch=16,cex=0.2)
-dev.off()
 
-
-###########################################################
 
 
 
