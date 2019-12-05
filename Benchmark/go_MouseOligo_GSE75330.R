99814e1a3b67c64cc7012d2a385fe4ecbd963d15
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-05 10:06:25 -0500

    Update go_MouseOligo_GSE75330.R

--- Benchmark/go_MouseOligo_GSE75330.R
+++ Benchmark/go_MouseOligo_GSE75330.R
@@ -73,21 +73,34 @@ DrawHeatMap<-function(TAG){
     while(i<=ncol(PCA)){
         MAT[i,THIS.R.PCA[,i]]=1
         i=i+1}
-
+    #######################
+    #MAT=t(apply(t(MAT),2,smooth))
+    ########################
+	
     library('ComplexHeatmap')
     library('circlize')
     library('seriation')
-
+    
     mat=MAT
     o.mat=mat
     col_fun =colorRamp2(c(0,1), c('white','#000080'))
-
+     
+    LLL=apply(mat,2,mean)
+    ha = HeatmapAnnotation(
+	M = anno_lines(LLL, add_points = FALSE,smooth=FALSE,
+		       gp = gpar(col = 'grey50',lwd=0.5),
+		      axis=FALSE),
+	name=c(''),
+	show_annotation_name=FALSE
+        )	
+	
     tiff(paste0("IMG/",TAG,".HEAT.tiff"),width=4,height=1,units='in',res=600)
     draw(Heatmap(o.mat,row_title='',name="",cluster_rows=FALSE,
         cluster_columns=FALSE,show_heatmap_legend=FALSE，
 	show_column_dend = FALSE, show_row_dend = FALSE, 
 	show_column_names=FALSE, show_row_names=FALSE,
-	col=col_fun, border = TRUE
+	col=col_fun, border = TRUE,
+        top_annotation = ha
 	))
     dev.off()
     }
