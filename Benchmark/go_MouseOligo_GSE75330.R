f4b640d690039db18653b438cc35ad3365456d9c
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-04 21:40:16 -0500

    Update go_MouseOligo_GSE75330.R

--- Benchmark/go_MouseOligo_GSE75330.R
+++ Benchmark/go_MouseOligo_GSE75330.R
@@ -19,7 +19,6 @@ pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min
 pbmc@meta.data$type=readRDS('GSE75330.LABEL.RDS')
 pbmc@meta.data$type[which(pbmc@meta.data$type=='Differentiation-committed oligodendrocyte precursors')]='Differentiation-committed OPC'
 
-
 ###############################################
 # Remove non-oligodendrocyte lineage cells
 used_cell_index=which( pbmc@meta.data$type != 'PPR')
@@ -30,35 +29,200 @@ TYPE=pbmc@meta.data$type[used_cell_index]
 # Analyze oligodendrocyte lineage cells
 pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
 pbmc@meta.data$type=TYPE
-##########################
 
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 all.genes <- rownames(pbmc)
 pbmc <- ScaleData(pbmc, features = all.genes)
-##########################
 
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
-pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 300)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 150)
 pbmc <- RunUMAP(pbmc, dims = 1:50)
 DimPlot(pbmc, reduction = "umap")
 saveRDS(pbmc,file='pbmc.RDS')
-
-###################
-
-
-
-
+######################################
 
 #############################
+# Reload oligodendrocyte lineage cells
+
 source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
-
 setwd('F:/Vector/data/MouseOligo_GSE75330')
-
 pbmc=readRDS('pbmc.RDS')
+#FeaturePlot(pbmc,features=c('Pdgfra','Bmp4','Sema4f','Mog'))
+
+VEC=pbmc@reductions$umap@cell.embeddings
+rownames(VEC)=colnames(pbmc)
+PCA= pbmc@reductions$pca@cell.embeddings
 
 
 
-FeaturePlot(pbmc,features=c('Pdgfra','Bmp4','Sema4f','Mog'))
+table(TYPE)
+#Differentiation-committed OPC         Mature Oligodendrocytes 
+#                            140                            2748 
+#Myelin-forming Oligodendrocytes   Newly-formed Oligodendrocytes 
+#                           1283                             512 
+#                            OPC 
+#                            310 
+
+
+###########################################
+#Draw heatmap
+
+DrawHeatMap<-function(TAG){
+
+    R.PCA=apply(PCA,2,rank)
+    THIS.INDEX=which(pbmc@meta.data$type==TAG)
+    THIS.R.PCA=R.PCA[THIS.INDEX,]
+
+
+    MAT=matrix(0,nrow=ncol(PCA),ncol=nrow(PCA))
+    rownames(MAT)=colnames(PCA)
+    colnames(MAT)=paste0('R_',1:nrow(PCA))
+    MAT[1:3,1:3]
+
+
+    i=1
+    while(i<=ncol(PCA)){
+        MAT[i,THIS.R.PCA[,i]]=1
+        i=i+1}
+
+    library('ComplexHeatmap')
+    library('circlize')
+    library('seriation')
+
+    mat=MAT
+    o.mat=mat
+    col_fun =colorRamp2(c(0,1), c('white','#000080'))
+
+    Heatmap(o.mat,row_title='',name="",cluster_rows=FALSE,
+        cluster_columns=FALSE,show_heatmap_legend=FALSE，
+	show_column_dend = FALSE, show_row_dend = FALSE, 
+	show_column_names=FALSE, show_row_names=FALSE,
+	col=col_fun, border = TRUE
+	)
+    tiff(paste0("IMG/",TAG,".HEAT.tiff"),width=6,height=2,units='in',res=600)
+    Heatmap(o.mat,row_title='',name="",cluster_rows=FALSE,
+        cluster_columns=FALSE,show_heatmap_legend=FALSE，
+	show_column_dend = FALSE, show_row_dend = FALSE, 
+	show_column_names=FALSE, show_row_names=FALSE,
+	col=col_fun, border = TRUE
+	)
+    dev.off()
+    }
+
+
+#########################################
+TAG='OPC'
+DrawHeatMap(TAG)
+###################################################
+TAG='Differentiation-committed OPC'
+
+R.PCA=apply(PCA,2,rank)
+THIS.INDEX=which(pbmc@meta.data$type==TAG)
+THIS.R.PCA=R.PCA[THIS.INDEX,]
+
+
+MAT=matrix(0,nrow=ncol(PCA),ncol=nrow(PCA))
+rownames(MAT)=colnames(PCA)
+colnames(MAT)=paste0('R_',1:nrow(PCA))
+MAT[1:3,1:3]
+
+
+i=1
+while(i<=ncol(PCA)){
+    MAT[i,THIS.R.PCA[,i]]=1
+    i=i+1}
+
+library('ComplexHeatmap')
+library('circlize')
+library('seriation')
+
+mat=MAT
+o.mat=mat
+col_fun =colorRamp2(c(0,1), c('white','#000080'))
+
+Heatmap(o.mat,row_title='',name="",cluster_rows=FALSE,
+        cluster_columns=FALSE,show_heatmap_legend=FALSE，
+	show_column_dend = FALSE, show_row_dend = FALSE, 
+	show_column_names=FALSE, show_row_names=FALSE,
+	col=col_fun, border = TRUE
+	)
+tiff(paste0("IMG/",TAG,".HEAT.tiff"),width=6,height=2,units='in',res=600)
+Heatmap(o.mat,row_title='',name="",cluster_rows=FALSE,
+        cluster_columns=FALSE,show_heatmap_legend=FALSE，
+	show_column_dend = FALSE, show_row_dend = FALSE, 
+	show_column_names=FALSE, show_row_names=FALSE,
+	col=col_fun, border = TRUE
+	)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
