5a88b7131ec5416386ceffd4bafc1422a331d2d3
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-07 00:49:04 -0500

    Update and rename go_HumanHepDiff_GSE81252.R to go_MouseGlial_GSE99933.R

---
+++ Benchmark/go_MouseGlial_GSE99933.R
@@ -0,0 +1,97 @@
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+
+setwd('F:/Vector/data/MouseGlial_GSE99933')
+
+
+
+D1=read.csv('E12.5_counts.txt',header=T,row.names=1,sep='\t')
+D2=read.csv('E13.5_counts.txt',header=T,row.names=1,sep='\t')
+
+DATA=.simple_combine(D1,D2)$combine
+TYPE=c(rep('E12.5',ncol(D1)),rep('E13.5',ncol(D2)))
+
+
+pbmc <- CreateSeuratObject(counts = DATA, project = "pbmc3k", min.cells = 0, min.features = 0)
+
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 5000)
+
+
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc),npcs = 150)
+#pbmc <- RunUMAP(pbmc, dims = 1:20, n.neighbors=10, min.dist=0.5,spread=1)
+pbmc <- RunUMAP(pbmc, dims = 1:50)
+
+pbmc@meta.data$type=TYPE
+DimPlot(pbmc, reduction = "umap",group.by='type',label=TRUE)
+saveRDS(pbmc,file='pbmc.RDS')
+
+##################
+
+
+
+VEC=pbmc@reductions$umap@cell.embeddings
+rownames(VEC)=colnames(pbmc)
+PCA= pbmc@reductions$pca@cell.embeddings
+
+
+OUT=vector.buildGrid(VEC, N=40,SHOW=TRUE)
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
+
+
+FeaturePlot(pbmc,features=c('Sox10','Htr3a','Th','Cartpt'))
+
+
+
+
+
+tiff(paste0("IMG/VECTOR.1.tiff"),width=4,height=4,units='in',res=600)
+par(mar=c(0,0,0,0))
+OUT=vector.buildGrid(VEC, N=40,SHOW=TRUE)
+dev.off()
+
+
+tiff(paste0("IMG/VECTOR.2.tiff"),width=4,height=4,units='in',res=600)
+par(mar=c(0,0,0,0))
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+dev.off()
+
+tiff(paste0("IMG/VECTOR.3.tiff"),width=4,height=4,units='in',res=600)
+par(mar=c(0,0,0,0))
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+dev.off()
+
+tiff(paste0("IMG/VECTOR.4.tiff"),width=4,height=4,units='in',res=600)
+par(mar=c(0,0,0,0))
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+dev.off()
+
+tiff(paste0("IMG/VECTOR.5.tiff"),width=4,height=4,units='in',res=600)
+par(mar=c(0,0,0,0))
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+dev.off()
+
+tiff(paste0("IMG/VECTOR.6.tiff"),width=4,height=4,units='in',res=600)
+par(mar=c(0,0,0,0))
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
+dev.off()
+
+
+
+
+tiff(paste0("IMG/Marker.tiff"),width=5,height=5,units='in',res=600)
+p <- FeaturePlot(pbmc, features=c('Sox10','Htr3a','Th','Cartpt'),order=TRUE, combine = FALSE)
+
+for(i in 1:length(p)) {
+  p[[i]] <- p[[i]] + NoLegend() + NoAxes()
+}
+
+
