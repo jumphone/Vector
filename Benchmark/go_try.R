57d9c68d41cc45e2b2d27480bbc9b80499960e69
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-07 23:28:53 -0500

    Update go_try.R

--- Benchmark/go_try.R
+++ Benchmark/go_try.R
@@ -2,3 +2,112 @@ source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
 setwd('F:/Vector/data/Try/')
 
 
+
+PCA.OUT.1=readRDS('F:/Vector/data/MouseIntestine_GSE92332/PCA.OUT_500.RDS')
+PCA.OUT.2=readRDS('F:/Vector/data/MouseOligo_GSE75330/PCA.OUT_500.RDS')
+
+ROT.1=PCA.OUT.1$rotation
+ROT.2=PCA.OUT.2$rotation
+
+ROT.1.NAME.UP=toupper(rownames(ROT.1))
+ROT.2.NAME.UP=toupper(rownames(ROT.2))
+
+USED.1=which(ROT.1.NAME.UP %in% names(which(table(ROT.1.NAME.UP )==1)))
+ROT.1=ROT.1[USED.1,]
+
+
+USED.2=which(ROT.2.NAME.UP %in% names(which(table(ROT.2.NAME.UP )==1)))
+ROT.2=ROT.2[USED.2,]
+
+
+rownames(ROT.1)=toupper(rownames(ROT.1))
+rownames(ROT.2)=toupper(rownames(ROT.2))
+
+colnames(ROT.1)=paste0('D1_',colnames(ROT.1))
+colnames(ROT.2)=paste0('D2_',colnames(ROT.2))
+
+
+ROT.1=ROT.1[,1:500]
+ROT.2=ROT.2[,1:500]
+
+
+
+ROT.COM=.simple_combine(ROT.1,ROT.2)$combine
+
+COR=cor(ROT.COM,method='spearman')
+SSS=apply(abs(COR),2,mean)
+
+USED=which(SSS<quantile(SSS,0.1))
+
+
+ROT.COM=ROT.COM[,USED]
+##########################################
+
+
+
+
+
+
+
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+setwd('F:/Vector/data/MouseIntestine_GSE92332/')
+pbmc=readRDS(file='pbmc_smart.RDS')
+
+
+
+ROT.COM=.simple_combine(ROT.1,ROT.2)$combine
+COR=cor(ROT.COM,method='spearman')
+SSS.1=apply(abs(COR[(ncol(ROT.1)+1):ncol(COR),]),2,mean)
+SSS.2=apply(abs(COR[1:ncol(ROT.1),]),2,mean)
+SSS=c(SSS.1[1:ncol(ROT.1)],SSS.2[(ncol(ROT.1)+1):ncol(COR)])
+USED=which(SSS<quantile(SSS,0.6))
+
+
+ROT.COM=ROT.COM[,USED]
+
+
+
+EXP=as.matrix(pbmc@assays$RNA@data)
+RN=toupper(rownames(EXP))
+EXP=EXP[which(RN %in% names(which(table(RN)==1))),]
+
+rownames(EXP)=toupper(rownames(EXP))
+
+COM=.simple_combine(EXP,ROT.COM)$combine
+
+EXP=COM[,1:ncol(EXP)]
+ROT=COM[,(1+ncol(EXP)):ncol(COM)]
+
+EXP=t(apply(t(EXP),2,scale))
+
+TMP=t(EXP) %*% ROT
+
+
+
+
+VEC=pbmc@reductions$umap@cell.embeddings
+rownames(VEC)=colnames(pbmc)
+PCA= TMP#pbmc@reductions$pca@cell.embeddings
+
+
+
+
+
+OUT=vector.buildGrid(VEC, N=40,SHOW=TRUE)
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
+
+
+
+
+
+
+
+
+
+
+
+
