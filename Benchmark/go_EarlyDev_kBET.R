8307f9fa4a0b76e8596d4813737eba84de2cfc70
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-11 11:23:03 -0500

    Update go_EarlyDev_kBET.R

--- Benchmark/go_EarlyDev_kBET.R
+++ Benchmark/go_EarlyDev_kBET.R
@@ -395,3 +395,87 @@ par(mar=c(0,0,0,0))
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL)
 dev.off()
 
+
+
+
+
+
+
+
+
+
+###########################
+
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+setwd('F:/Vector/data/kBET/')
+
+pbmc=readRDS(file='pbmc.RDS')
+
+VEC=pbmc@reductions$umap@cell.embeddings
+rownames(VEC)=colnames(pbmc)
+PCA= pbmc@reductions$pca@cell.embeddings
+
+
+OUT=vector.buildGrid(VEC, N=20,SHOW=TRUE)
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+
+
+pdf('./IMG/TEST_nCount_RNA.pdf')
+OUT$VALUE=pbmc@meta.data$nCount_RNA
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=FALSE)
+dev.off()
+
+
+pdf('./IMG/TEST_nFeature_RNA.pdf')
+OUT$VALUE=pbmc@meta.data$nFeature_RNA
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=FALSE)
+dev.off()
+
+
+
+
+pdf('./IMG/TEST_VGENE.pdf')
+VGENE=t(as.matrix(pbmc@assays$RNA@data[which(rownames(pbmc) %in% VariableFeatures(pbmc)),]))
+OUT=vector.getValue(OUT, VGENE, SHOW=TRUE)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=FALSE)
+dev.off()
+
+
+
+pdf('./IMG/TEST_SD_PCA.pdf')
+#VGENE=t(as.matrix(pbmc@assays$RNA@data[which(rownames(pbmc) %in% VariableFeatures(pbmc)),]))
+#OUT=vector.getValue(OUT, VGENE, SHOW=TRUE)
+OUT$VALUE=apply(PCA,1,sd)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=FALSE)
+dev.off()
+
+
+pdf('./IMG/TEST_VAR_PCA.pdf')
+#VGENE=t(as.matrix(pbmc@assays$RNA@data[which(rownames(pbmc) %in% VariableFeatures(pbmc)),]))
+#OUT=vector.getValue(OUT, VGENE, SHOW=TRUE)
+OUT$VALUE=apply(PCA,1,var)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL, SHOW.SUMMIT=FALSE)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
