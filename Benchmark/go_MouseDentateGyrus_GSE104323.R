6e237a020178c00764498c503531a10a496c32d0
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-26 11:47:15 -0500

    Update go_MouseDentateGyrus_GSE104323.R

--- Benchmark/go_MouseDentateGyrus_GSE104323.R
+++ Benchmark/go_MouseDentateGyrus_GSE104323.R
@@ -118,6 +118,10 @@ OUT=vector.gridValue(OUT,SHOW=TRUE)
 OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=40)
 
+tiff(paste0("IMG/NEW_SMART.VECTOR_TRY.6.tiff"),width=4,height=4,units='in',res=600)
+par(mar=c(0,0,0,0))
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=70,OL=1.5,AW=1,AC='black')
+dev.off()
 
 tiff(paste0("IMG/NEW_VECTOR.6.tiff"),width=4,height=4,units='in',res=600)
 par(mar=c(0,0,0,0))
