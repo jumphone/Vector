8ff08c4a5338b44d63ea278e1399121d71dd7ea8
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-26 11:52:05 -0500

    Update go_EarlyDev_kBET.R

--- Benchmark/go_EarlyDev_kBET.R
+++ Benchmark/go_EarlyDev_kBET.R
@@ -83,7 +83,7 @@ OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, OL=2,COL=OUT$COL)
 
 
-tiff(paste0("IMG/NEW_SMART.VECTOR_TRY.6.tiff"),width=4,height=4,units='in',res=600)
+tiff(paste0("IMG/NEW_VECTOR_TRY.6.tiff"),width=4,height=4,units='in',res=600)
 par(mar=c(0,0,0,0))
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=40,OL=2,AW=1.5,AC='black',CEX=1)
 dev.off()
