fb1b7ed332ec10a5b4b89942a0efb954551dbf56
Zhang, Feng <15110700005@fudan.edu.cn>
2019-12-27 01:57:53 -0500

    Update go_MouseDentateGyrus_GSE104323.R

--- Benchmark/go_MouseDentateGyrus_GSE104323.R
+++ Benchmark/go_MouseDentateGyrus_GSE104323.R
@@ -120,7 +120,7 @@ OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=40)
 
 tiff(paste0("IMG/NEW_VECTOR_TRY.6.tiff"),width=4,height=4,units='in',res=600)
 par(mar=c(0,0,0,0))
-OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=70,OL=1.5,AW=1,AC='black')
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=75,BD=F,OL=1.5, AW=1,AC='black')
 dev.off()
 
 tiff(paste0("IMG/NEW_VECTOR.6.tiff"),width=4,height=4,units='in',res=600)
