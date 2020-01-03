864943d013bb3d11ab825217a67c204be7e9f479
Zhang, Feng <15110700005@fudan.edu.cn>
2020-01-03 17:58:22 -0500

    Update go_MouseNeuralCrest_GSE129114.R

--- Benchmark/go_MouseNeuralCrest_GSE129114.R
+++ Benchmark/go_MouseNeuralCrest_GSE129114.R
@@ -27,14 +27,26 @@ FeaturePlot(pbmc,features = c('Mafb','Olig3','Dlx5','Ret'))
 
 saveRDS(pbmc,file='pbmc_D4.RDS')
 
+#########################
 
 
+
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+setwd('F:/Vector/data/MouseNeuralCrest_GSE129114')
+
+pbmc=readRDS('pbmc_D4.RDS')
+
+VEC = pbmc@reductions$umap@cell.embeddings
+rownames(VEC) = colnames(pbmc)
+PCA = pbmc@reductions$pca@cell.embeddings
+
+#PCA=vector.rankPCA(PCA)
 
 OUT=vector.buildGrid(VEC, N=10,SHOW=TRUE)
 OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
 OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
 OUT=vector.gridValue(OUT,SHOW=TRUE)
-OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.5,SHOW=TRUE)
 OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=90)
 
 tiff(paste0("IMG/VECTOR.1.tiff"),width=4,height=4,units='in',res=600)
