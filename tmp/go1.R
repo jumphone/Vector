52832cd67d365d6f1db681a7e60064ffbae470db
Zhang, Feng <15110700005@fudan.edu.cn>
2020-01-03 11:34:32 -0500

    Create go1.R

---
+++ tmp/go1.R
@@ -0,0 +1,21 @@
+
+
+##########################################
+source('https://raw.githubusercontent.com/jumphone/BEER/master/BEER.R')
+setwd('F:/Vector/data/MouseDentateGyrus_GSE104323/')
+pbmc=readRDS(file='pbmc.RDS')
+
+VEC=pbmc@reductions$umap@cell.embeddings
+rownames(VEC)=colnames(pbmc)
+PCA= pbmc@reductions$pca@cell.embeddings
+
+
+
+
+
+OUT=vector.buildGrid(VEC, N=40,SHOW=TRUE)
+OUT=vector.buildNet(OUT, CUT=1, SHOW=TRUE)
+OUT=vector.getValue(OUT, PCA, SHOW=TRUE)
+OUT=vector.gridValue(OUT,SHOW=TRUE)
+OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
+OUT=vector.drawArrow(OUT,P=0.9,SHOW=TRUE, COL=OUT$COL,AL=40)
