389646ac1bc4ddecda4078e3b857dae5c2f3de0c
Zhang, Feng <15110700005@fudan.edu.cn>
2020-06-07 15:26:10 +0800

    Update go_MouseBoneMarrow_GSE109989.R

--- Benchmark/go_MouseBoneMarrow_GSE109989.R
+++ Benchmark/go_MouseBoneMarrow_GSE109989.R
@@ -50,6 +50,24 @@ OUT=vector.gridValue(OUT,SHOW=TRUE)
 OUT=vector.autoCenter(OUT,UP=0.9,SHOW=TRUE)
 
 
+#setwd('F:/Vector/data/MouseBoneMarrow_GSE109989/')
+#pbmc=readRDS(file='pbmc.RDS')
+VVV=OUT$VALUE
+TTT=rep(NA, length(VVV))
+DATA=pbmc@assays$RNA@counts
+TTT[which(DATA[which(rownames(DATA)=='Cenpa'),] >0)]='Cenpa +'
+TTT[which(DATA[which(rownames(DATA)=='Il1b'),] >0)]='Il1b +'
+
+USED=which(!is.na(TTT))
+TTT=TTT[USED]
+VVV=VVV[USED]
+
+
+TTT=as.factor(TTT)
+TTT=factor(TTT , levels=c('Cenpa +','Il1b +'))
+
+boxplot(VVV~TTT,outline=FALSE,xlab='',ylab='',las=2)
+
 
 
 tiff(paste0("IMG/NEW_VECTOR_TRY.6.tiff"),width=4,height=4,units='in',res=600)
